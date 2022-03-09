7df7ef9d9c2a3585a38eeca42f489ccb8a2f7604
Konsta Nenonen <konstanenonen@protonmail.com>
2022-03-09 17:32:11 +0200

    Finished sevents task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  return Number(((farenheit - 32) * (5/9)).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  return Number((celcius * (9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
