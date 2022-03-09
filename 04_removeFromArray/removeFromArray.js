6144b65e472b93753730ee7527e4ac37d467deb5
Konsta Nenonen <konstanenonen@protonmail.com>
2022-03-09 16:42:08 +0200

    Finished fourth task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
+  const args = [...arguments];
+  const array = args[0];
+  const targets = args.slice(1);
 
+  return array.filter(item => !targets.includes(item));
 };
 
 // Do not edit below this line
