b78975c6759a14d8b0370497231c5655f5093cbe
Konsta Nenonen <konstanenonen@protonmail.com>
2022-03-12 18:41:20 +0200

    Finished task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((prev, cur) => (prev + cur),0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prev, cur) => (prev * cur),1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) return 1;
+
+  let factorial = number;
+	for (let i = number - 1; i >= 1; i--) {
+    factorial = i * factorial;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
