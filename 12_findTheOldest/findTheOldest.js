4af26c49aedeecf5654c36134916448f1e589af6
Konsta Nenonen <konstanenonen@protonmail.com>
2022-03-12 20:43:42 +0200

    Finished task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  return array.reduce((previousValue, currentValue) => {
+    if (currentValue.yearOfDeath === undefined) {
+      const currentValueAge = new Date().getFullYear() - currentValue.yearOfBirth;
+      const previousValueAge = previousValue.yearOfDeath - previousValue.yearOfBirth;
+      return (currentValueAge > previousValueAge) ? currentValue : previousValue;
+    } else {
+      const currentValueAge = currentValue.yearOfDeath - currentValue.yearOfBirth;
+      if (previousValue.yearOfDeath === undefined) {
+        const previousValueAge = new Date().getFullYear() - previousValue.yearOfBirth;
+        return (currentValueAge > previousValueAge) ? currentValue : previousValue;
+      } else {
+        const previousValueAge = previousValue.yearOfDeath - previousValue.yearOfBirth;
+        return (currentValueAge > previousValueAge) ? currentValue : previousValue;
+      }
+    } 
+  })
 };
 
 // Do not edit below this line
